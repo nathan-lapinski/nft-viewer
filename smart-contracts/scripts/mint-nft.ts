7d4cca9ea858ffb2f454aee18781b8dbf658ffdb
nate <n4t311@gmail.com>
2022-08-09 22:41:31 +0900

    feat: adds alchemy info and deploy script

---
+++ smart-contracts/scripts/mint-nft.ts
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+// Get Alchemy API Key
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy Provider
+const provider = new ethers.providers.AlchemyProvider('goerli', API_KEY)
