3a6de6d45654b5744ffdab8f784ce9fafdb2e83f
nate <n4t311@gmail.com>
2022-08-15 20:13:47 +0900

    feat: adds http server to mint nft

--- smart-contracts/scripts/mint-nft.ts
+++ smart-contracts/scripts/mint-nft.ts
@@ -2,6 +2,11 @@ const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, "../.env") });
 const ethers = require('ethers');
 const contract = require("../artifacts/contracts/MintNFT.sol/MintNFT.json");
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Get Alchemy API Key
 const API_KEY = process.env.API_KEY;
@@ -22,16 +27,43 @@ const myNftContract = new ethers.Contract(contractAddress, abi, signer)
 
 const tokenUri = 'https://gateway.pinata.cloud/ipfs/QmQ5pt4GTqzwXRKniwsdJc1RM3cTkG5tyVcXzsuwZ1USWi';
 
-// Call mintNFT function
-const mintNFT = async () => {
-    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
-    await nftTxn.wait()
-    console.log(`NFT Minted! Check it out at: https://goerli.etherscan.io/tx/${nftTxn.hash}`)
-}
+app.get('/', (req, res) => {
+    res.send('success from NFT');
+});
 
-mintNFT()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+app.post('/mint', async (req, res) => {
+
+    console.log(req.body)
+
+    const cid = req.body.cid;
+
+    if (!cid) {
+        return res.send('no cid found')
+    }
+
+    // TODO: should not store gateway on chain - just CID
+    const uri = `https://gateway.pinata.cloud/ipfs/${cid}`;
+
+    console.log('sending to ethereum...')
+
+    // Call mintNFT function
+    const mintNFT = async () => {
+        let nftTxn = await myNftContract.mintNFT(signer.address, uri)
+        await nftTxn.wait()
+        console.log(`NFT Minted! Check it out at: https://goerli.etherscan.io/tx/${nftTxn.hash}`)
+    }
+
+    const val = await mintNFT()
+        // .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
+    console.log(val);
+    res.send(val)
+});
+
+app.listen(4000, () => {
+    console.log(`server listening on port ${4000}`);
+});
+
